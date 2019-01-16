49c3f91ed899bf176b5c90da95a8a644beb3e2bf
Montague, Brent <bmontague@cvent.com>
2019-01-15 19:34:06 -0500

    use abstract class test

---
+++ src/main/java/com/brentm5/AbstractRunner.java
@@ -0,0 +1,51 @@
+package com.brentm5;
+
+import datadog.trace.api.Trace;
+import io.opentracing.Scope;
+import io.opentracing.Tracer;
+import io.opentracing.util.GlobalTracer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Random;
+import java.util.UUID;
+
+abstract class AbstractRunner implements Runnable {
+
+    private static Logger logger = LogManager.getLogger(AbstractRunner.class);
+    private final Tracer tracer = GlobalTracer.get();
+
+    public void run(){
+        while(true) {
+            try (Scope scope = tracer.buildSpan("message.process").startActive(true)) {
+                String nextMessage = getNextMessage();
+                scope.span().setBaggageItem("message.id", nextMessage);
+                handleMessage(nextMessage);
+            }
+        }
+    }
+
+    @Trace
+    protected abstract void consume(String message);
+
+    @Trace
+    private void handleMessage(String message) {
+        try {
+            consume(message);
+        } catch (Exception e) {
+        }
+    }
+
+    @Trace
+    private String getNextMessage() {
+        try {
+            Thread.sleep(getRandomInt(500));
+        } catch (Exception e) {
+        }
+        return UUID.randomUUID().toString();
+    }
+
+    protected int getRandomInt(int max) {
+        return new Random().nextInt(max);
+    }
+}
--- src/main/java/com/brentm5/Runner.java
+++ src/main/java/com/brentm5/Runner.java
@@ -1,50 +1,24 @@
 package com.brentm5;
 
 import datadog.trace.api.Trace;
-import io.opentracing.Scope;
 import io.opentracing.Tracer;
 import io.opentracing.util.GlobalTracer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.Random;
-import java.util.UUID;
-
-public class Runner implements Runnable {
+public class Runner extends AbstractRunner {
 
     private static Logger logger = LogManager.getLogger(Runner.class);
     private final Tracer tracer = GlobalTracer.get();
 
-    public void run(){
-        while(true) {
-            try (Scope scope = tracer.buildSpan("message.process").startActive(true)) {
-                String nextMessage = getNextMessage();
-                scope.span().setBaggageItem("message.id", nextMessage);
-                processMessage(nextMessage);
-            }
-        }
-    }
-
     @Trace
-    private void processMessage(String message) {
+    @Override
+    protected void consume(String message) {
         String threadId = String.format("Thread: %s", Thread.currentThread().getId());
         System.out.println(String.format("%s - %s", threadId, message));
         try {
             Thread.sleep(getRandomInt(1000));
         } catch (Exception e) {
         }
-    }
-
-    @Trace
-    private String getNextMessage() {
-        try {
-            Thread.sleep(getRandomInt(500));
-        } catch (Exception e) {
-        }
-        return UUID.randomUUID().toString();
-    }
-
-    private int getRandomInt(int max) {
-        return new Random().nextInt(max);
     }
 }
